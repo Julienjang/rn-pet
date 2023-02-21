34a556f3754d1b25644503f4ff0926c75af24870
Julienjang <115388594+Julienjang@users.noreply.github.com>
2023-02-21 23:32:12 +0900

    서버 연결전이여

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
+import { StatusBar } from 'expo-status-bar';
 import Navigation from './navigations';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect, useState } from 'react';
+import { View } from 'react-native';
+import { Asset } from 'expo-asset';
+import axios from 'axios';
 
 const App = () => {
-  return <Navigation />;
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await Asset.fromModule(
+          require('../assets/petCover.png')
+        ).downloadAsync();
+      } catch (e) {
+        // eslint-disable-next-line no-console
+        console.log(e);
+      } finally {
+        setIsReady(true);
+      }
+    })();
+  }, []);
+
+  const onReady = async () => {
+    if (isReady) {
+      await SplashScreen.hideAsync();
+    }
+  };
+
+  if (!isReady) {
+    return null;
+  }
+
+  return (
+    <View style={{ flex: 1 }} onLayout={onReady}>
+      <StatusBar style={'dark'} />
+      <Navigation />
+    </View>
+  );
 };
 
 export default App;
