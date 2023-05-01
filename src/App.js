285e45ed85e027857450d7da21aee7eb0cbbb5b5
JulienJang <big00124@naver.com>
2023-05-01 16:18:36 +0900

    test commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import { StatusBar } from 'react-native';
+import Navigation from './navigations';
+
+const App = () => {
+  <StatusBar style={'dark'} />;
+  return <Navigation />;
+};
+
+export default App;
